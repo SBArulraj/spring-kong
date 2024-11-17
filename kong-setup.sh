628a67974112642024733a469d85c3a5684312fd
SBArulraj <esarulraj@gmail.com>
2024-11-17 23:19:05 +0530

    Kong change

--- kong-setup.sh
+++ kong-setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Register the Spring Boot service with Kong
+# Register the Spring Boot service with Kong Gateway (in case you prefer manual setup)
 
-KONG_ADMIN_URL="http://localhost:8002"
+KONG_ADMIN_URL="http://localhost:8001"
 
-# Create a service
+# Register a new service
 curl -i -X POST ${KONG_ADMIN_URL}/services/ \
   --data "name=springboot-service" \
   --data "url=http://localhost:8080/hello"
 
-# Create a route
+# Add a route to the service
 curl -i -X POST ${KONG_ADMIN_URL}/services/springboot-service/routes \
-  --data "paths[]=/hello" \
+  --data "paths[]=/api" \
   --data "strip_path=true"
