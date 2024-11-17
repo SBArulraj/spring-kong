04b63792f2ced4bda9537931453adc67022ca97f
SBArulraj <esarulraj@gmail.com>
2024-11-17 16:37:31 +0530

    Initial Push

---
+++ src/main/java/com/example/kongtesting/Kong_Testing_Arulraj/KongTestingArulrajApplication.java
@@ -0,0 +1,13 @@
+package com.example.kongtesting.Kong_Testing_Arulraj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongTestingArulrajApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongTestingArulrajApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kongtesting/Kong_Testing_Arulraj/KongTestingArulrajApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kongtesting.Kong_Testing_Arulraj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongTestingArulrajApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
