5d1cdab93a231ad5480b29ec921ec50de78516bf
SBArulraj <esarulraj@gmail.com>
2024-11-18 08:05:29 +0530

    Kong change-postgres:15 port change for Kong 7001

--- kong-setup.sh
+++ kong-setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Register the Spring Boot service with Kong Gateway (in case you prefer manual setup)
 
-KONG_ADMIN_URL="http://192.168.1.11:7001"
+KONG_ADMIN_URL="http://localhost:7001"
 
 # Register a new service
 curl -i -X POST ${KONG_ADMIN_URL}/services/ \
   --data "name=springboot-service" \
-  --data "url=http://192.168.1.11:8080/hello"
+  --data "url=http://localhost:8080/hello"
 
 # Add a route to the service
 curl -i -X POST ${KONG_ADMIN_URL}/services/springboot-service/routes \
